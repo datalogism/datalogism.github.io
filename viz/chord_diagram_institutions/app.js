cc5227ff27c5c5610333f33dbd24cec5b877154e
c_ringwald <celian.ringwald@hotmail.fr>
2022-06-12 19:44:20 +0200

    add PH work

---
+++ viz/chord_diagram_institutions/app.js
@@ -0,0 +1,132 @@
+////////////////////////////////////////////////////////////
+//////////////////////// Set-Up ////////////////////////////
+////////////////////////////////////////////////////////////
+
+var margin = {left:90, top:90, right:90, bottom:90},
+	width = Math.min(window.innerWidth, 700) - margin.left - margin.right,
+    height = Math.min(window.innerWidth, 700) - margin.top - margin.bottom,
+    innerRadius = Math.min(width, height) * .39,
+    outerRadius = innerRadius * 1.1;
+	
+var Names = ['Inria', 'Ruhr-Universität Bochum', ' University of Sussex', ' Universidade Federal de São Paulo', 'Université Bordeaux Montaigne', 'Florida State University', 'University of Oxford', ' Faculdade de Ciências Sociais e Humanas da Universidade Nova de Lisboa', ' Fundação Getulio Vargas', 'Indiana University', 'University of Illinois', 'George Mason University', 'Denison University', 'University of Denver', ' "King\'s College London"', 'Wayne State University', 'Escola de Ciências Sociais FGV/CPDOC', 'El Colegio de Michoacán']
+colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+	opacityDefault = 0.8;
+
+var matrix =[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 2, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2]]/////////// Create scale and layout functions //////////////
+////////////////////////////////////////////////////////////
+
+var colors = d3.scale.ordinal()
+    .domain(d3.range(Names.length))
+	.range(colors);
+
+var chord = d3.layout.chord()
+    .padding(.15)
+    .sortChords(d3.descending)
+	.matrix(matrix);
+		
+var arc = d3.svg.arc()
+    .innerRadius(innerRadius*1.01)
+    .outerRadius(outerRadius);
+
+var path = d3.svg.chord()
+	.radius(innerRadius);
+	
+////////////////////////////////////////////////////////////
+////////////////////// Create SVG //////////////////////////
+////////////////////////////////////////////////////////////
+	
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+	.append("g")
+    .attr("transform", "translate(" + (width/2 + margin.left) + "," + (height/2 + margin.top) + ")");
+
+////////////////////////////////////////////////////////////
+/////////////// Create the gradient fills //////////////////
+////////////////////////////////////////////////////////////
+
+//Function to create the unique id for each chord gradient
+function getGradID(d){ return "linkGrad-" + d.source.index + "-" + d.target.index; }
+
+//Create the gradients definitions for each chord
+var grads = svg.append("defs").selectAll("linearGradient")
+	.data(chord.chords())
+   .enter().append("linearGradient")
+    //Create the unique ID for this specific source-target pairing
+	.attr("id", getGradID)
+	.attr("gradientUnits", "userSpaceOnUse")
+	//Find the location where the source chord starts
+	.attr("x1", function(d,i) { return innerRadius * Math.cos((d.source.endAngle-d.source.startAngle)/2 + d.source.startAngle - Math.PI/2); })
+	.attr("y1", function(d,i) { return innerRadius * Math.sin((d.source.endAngle-d.source.startAngle)/2 + d.source.startAngle - Math.PI/2); })
+	//Find the location where the target chord starts 
+	.attr("x2", function(d,i) { return innerRadius * Math.cos((d.target.endAngle-d.target.startAngle)/2 + d.target.startAngle - Math.PI/2); })
+	.attr("y2", function(d,i) { return innerRadius * Math.sin((d.target.endAngle-d.target.startAngle)/2 + d.target.startAngle - Math.PI/2); })
+
+//Set the starting color (at 0%)
+grads.append("stop")
+	.attr("offset", "0%")
+	.attr("stop-color", function(d){ return colors(d.source.index); });
+
+//Set the ending color (at 100%)
+grads.append("stop")
+	.attr("offset", "100%")
+	.attr("stop-color", function(d){ return colors(d.target.index); });
+		
+////////////////////////////////////////////////////////////
+////////////////// Draw outer Arcs /////////////////////////
+////////////////////////////////////////////////////////////
+
+var outerArcs = svg.selectAll("g.group")
+	.data(chord.groups)
+	.enter().append("g")
+	.attr("class", "group")
+	.on("mouseover", fade(.1))
+	.on("mouseout", fade(opacityDefault));
+
+outerArcs.append("path")
+	.style("fill", function(d) { return colors(d.index); })
+	.attr("d", arc);
+	
+////////////////////////////////////////////////////////////
+////////////////////// Append Names ////////////////////////
+////////////////////////////////////////////////////////////
+
+//Append the label names on the outside
+outerArcs.append("text")
+  .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
+  .attr("dy", ".35em")
+  .attr("class", "titles")
+  .attr("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+  .attr("transform", function(d) {
+		return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+		+ "translate(" + (outerRadius + 10) + ")"
+		+ (d.angle > Math.PI ? "rotate(180)" : "");
+  })
+  .text(function(d,i) { return Names[i]; });
+	
+////////////////////////////////////////////////////////////
+////////////////// Draw inner chords ///////////////////////
+////////////////////////////////////////////////////////////
+  
+svg.selectAll("path.chord")
+	.data(chord.chords)
+	.enter().append("path")
+	.attr("class", "chord")
+	//change the fill to reference the unique gradient ID of the source-target combination
+	.style("fill", function(d){ return "url(#" + getGradID(d) + ")"; })
+	.style("opacity", opacityDefault)
+	.attr("d", path);
+
+////////////////////////////////////////////////////////////
+////////////////// Extra Functions /////////////////////////
+////////////////////////////////////////////////////////////
+
+//Returns an event handler for fading a given chord group.
+function fade(opacity) {
+  return function(d,i) {
+    svg.selectAll("path.chord")
+        .filter(function(d) { return d.source.index != i && d.target.index != i; })
+		.transition()
+        .style("opacity", opacity);
+  };
+}//fade
